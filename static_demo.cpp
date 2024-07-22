f33501a45738472227002d0150cf49d0f7ce9f2c
Karishma Wanchoo <122223066+Karishmawanchoo@users.noreply.github.com>
2024-07-22 12:16:13 +0530

    Add files via upload

---
+++ static_demo.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+void example(){
+    static int count=0;
+    count++;
+    cout << "Count: " << count << endl;
+}
+int main(){
+    example();
+    return 0;
+}
